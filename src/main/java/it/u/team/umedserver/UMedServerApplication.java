2a62e6a247d5c8c1ae7868ab29fa99ba60ea0731
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-10-19 00:29:02 -0700

    v1

---
+++ src/main/java/it/u/team/umedserver/UMedServerApplication.java
@@ -0,0 +1,13 @@
+package it.u.team.umedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UMedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UMedServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/u/team/umedserver/UMedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.u.team.umedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UMedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
